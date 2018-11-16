edc0857f57fd98d58a5e50066bf55cefd1b34d0d
Julien LE JEUNE <mail.julien.le.jeune@gmail.com>
2018-11-16 16:03:13 +0100

    Update pain.py

--- OVHJabberBot/commands/pain.py
+++ OVHJabberBot/commands/pain.py
@@ -13,8 +13,8 @@ class Pain(object):
     def non(self, username):
         """ Annuler la commande d'une baguette """
         if username in self.orders:
-            self.orders.delete(username)
+            self.orders.remove(username)
 
-    def list_orders(self):
+    def liste_orders(self):
         """ Liste les gens qui veulent une baguette """
         return 'Liste des gens qui veulent une baguette: {}'.format(' '.join([name for name in self.orders]))
